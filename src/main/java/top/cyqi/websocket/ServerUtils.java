011f648232fa60f2594816056748104453c8f6df
liujiaqi7998 <1792498113@qq.com>
2022-05-11 15:27:33 +0800

    显示玩家

--- src/main/java/top/cyqi/websocket/ServerUtils.java
+++ src/main/java/top/cyqi/websocket/ServerUtils.java
@@ -6,8 +6,11 @@ import emu.grasscutter.game.player.Player;
 import io.javalin.websocket.WsMessageContext;
 import top.cyqi.GrasscuttersWebDashboard;
 import top.cyqi.websocket.json.BaseData;
+import top.cyqi.websocket.json.PlayerData;
 import top.cyqi.websocket.json.WSData;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 
@@ -25,7 +28,21 @@ public class ServerUtils {
             }
             case "Player" -> {
                 Map<Integer, Player> playerMap = GrasscuttersWebDashboard.getGameServer().getPlayers();
-
+                //循环遍历所有玩家
+                List<PlayerData> playerDatas = new ArrayList<>();
+                for (Map.Entry<Integer, Player> entry : playerMap.entrySet()) {
+                    Player player = entry.getValue();
+                    PlayerData playerData = new PlayerData();
+                    playerData.uid = player.getUid();
+                    playerData.signature = player.getSignature();
+                    playerData.nickname = player.getNickname();
+                    playerData.headImage = player.getHeadImage();
+                    playerData.worldLevel = player.getWorldLevel();
+                    playerData.Level = player.getLevel();
+                    playerData.pos = player.getPos().toString();
+                    playerDatas.add(playerData);
+                }
+                wsMessageContext.send(new WSData("PlayerList", playerDatas));
             }
         }
     }
---
+++ src/main/java/top/cyqi/websocket/json/PlayerData.java
@@ -0,0 +1,16 @@
+package top.cyqi.websocket.json;
+
+import emu.grasscutter.game.Account;
+import emu.grasscutter.game.player.PlayerBirthday;
+
+public class PlayerData {
+
+    public int uid;
+    public String nickname;
+    public String signature;
+    public int headImage;
+    public int worldLevel;
+    public String pos;
+    public int Level;
+
+}
