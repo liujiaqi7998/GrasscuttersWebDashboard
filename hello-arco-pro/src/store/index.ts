759e690ac2bfa53a575741fa8be49a6d975dd632
liujiaqi7998 <1792498113@qq.com>
2022-05-16 23:46:51 +0800

    新的变更列表

--- hello-arco-pro/src/store/index.ts
+++
@@ -1,9 +0,0 @@
-import { createPinia } from 'pinia';
-import useAppStore from './modules/app';
-import useUserStore from './modules/user';
-import useTabBarStore from './modules/tab-bar';
-
-const pinia = createPinia();
-
-export { useAppStore, useUserStore, useTabBarStore };
-export default pinia;
